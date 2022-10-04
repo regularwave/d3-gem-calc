18ffe79a4b7e3b8e48c129711d378e4486a0afd2
regularwave <regularwave@gmail.com>
2022-10-04 13:56:08 -0400

    javascript and fix everything up

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
-    input.addEventListener('change', (a) => dothething(a) )
+    input.addEventListener('change', (a) => calcGemRecipe(a))
 });
 
-function dothething(gemArray) {
-    document.getElementById('recipeCard').innerHTML = document.getElementById('recipeCard').innerHTML + '<br>' + gemArray.target.id + " " + gemArray.target.value;
-    // console.log(gemArray.target.id + " " + gemArray.target.value);
+function calcGemRecipe() {
 
-    console.log(gemArray)
+    const gems = {
+        "wFR": document.querySelector('#wantFR').valueAsNumber,
+        "hR": document.querySelector('#haveR').valueAsNumber,
+        "hFI": document.querySelector('#haveFI').valueAsNumber,
+        "hI": document.querySelector('#haveI').valueAsNumber,
+        "hM": document.querySelector('#haveM').valueAsNumber,
+        "rR": 0, "rFI": 0, "rI": 0, "rM": 0
+    };
+
+    gems.rR = Math.max(0, gems.wFR * 3 - gems.hR);
+    gems.rFI = Math.max(0, gems.rR * 3 - gems.hFI);
+    gems.rI = Math.max(0, gems.rFI * 3 - gems.hI);
+    gems.rM = Math.max(0, gems.rI * 3 - gems.hM);
+
+    printGemRecipe(gems);
+}
+
+function printGemRecipe(gems) {
+    const recipeCard = document.getElementById('recipeCard');
+    recipeCard.innerHTML = "";
+
+    if (gems.rM != 0) {
+        recipeCard.innerHTML = "You need " + (gems.rM) + " more Marquise gem" + (gems.rM == 1 ? "" : "s") + ".";
+    } else {
+        if (gems.rI > 0) { recipeCard.innerHTML = recipeCard.innerHTML + '<p>' + "Create " + gems.rI + " Imperial gem" + (gems.rI == 1 ? "" : "s") + "." + '</p>'; }
+        if (gems.rFI > 0) { recipeCard.innerHTML = recipeCard.innerHTML + '<p>' + "Create " + gems.rFI + " Flawless Imperial gem" + (gems.rFI == 1 ? "" : "s") + "." + '</p>'; }
+        if (gems.rR > 0) { recipeCard.innerHTML = recipeCard.innerHTML + '<p>' + "Create " + gems.rR + " Royal gem" + (gems.rR == 1 ? "" : "s") + "." + '</p>'; }
+        recipeCard.innerHTML = recipeCard.innerHTML + '<p>' + "Create " + gems.wFR + " Flawless Royal gem" + (gems.wFR == 1 ? "" : "s") + "." + '</p>';
+    }
 }
